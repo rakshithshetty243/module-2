83c67b9be0fa5d990ec480ddec43f2e8ab204c2f
rakshithshetty243 <160031239+rakshithshetty243@users.noreply.github.com>
2024-02-25 23:56:27 +0530

    Update consts.go

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "hypersdk"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "rakshith"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "RK"
 )
 
 var ID ids.ID
